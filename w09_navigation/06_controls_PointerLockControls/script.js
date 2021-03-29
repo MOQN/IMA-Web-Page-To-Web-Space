7a7e756764596c05307b13a15877f9a4b9dabe7f
moqn <moonjunghyun@gmail.com>
2021-03-29 02:43:39 -0700

    uploaded example codes for navigation

---
+++ w09_navigation/06_controls_PointerLockControls/script.js
@@ -0,0 +1,243 @@
+const WORLD_HALF = 200;
+
+let plane;
+// for controls movements
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = true;
+let moveVelocity = 5;
+let jumpVelocity = 0;
+let jumpAccel = 10;
+let fallAccel = 0.3;
+
+function setupTHREE() {
+  // plane
+  plane = getPlane();
+  plane.position.y = -WORLD_HALF / 2;
+  plane.rotation.x = -PI / 2;
+  scene.add(plane);
+
+  // boxes
+  for (let i = 0; i < 100; i++) {
+    let box = getBox();
+
+    box.position.x = random(-WORLD_HALF, WORLD_HALF);
+    box.position.y = random(-WORLD_HALF / 2, WORLD_HALF);
+    box.position.z = random(-WORLD_HALF, WORLD_HALF);
+
+    box.rotation.x = random(TWO_PI);
+    box.rotation.y = random(TWO_PI);
+    box.rotation.z = random(TWO_PI);
+
+    const size = random(1, 20);
+    box.scale.x = size;
+    box.scale.y = size;
+    box.scale.z = size;
+
+    box.material.transparent = true;
+    box.material.opacity = random(0.4, 0.7);
+
+    scene.add(box);
+  }
+}
+
+function updateTHREE() {
+  // PointerLockControls
+
+  if (moveForward) {
+    controls.moveForward(moveVelocity);
+  } else if (moveBackward) {
+    controls.moveForward(-moveVelocity);
+  }
+  if (moveLeft) {
+    controls.moveRight(-moveVelocity);
+  } else if (moveRight) {
+    controls.moveRight(moveVelocity);
+  }
+  // jump
+  controls.getObject().position.y += jumpVelocity;
+
+  if (controls.getObject().position.y > 0) {
+    jumpVelocity -= fallAccel;
+  } else {
+    controls.getObject().position.y = 0;
+    jumpVelocity = 0;
+    canJump = true;
+  }
+
+
+}
+
+function getBox() {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial();
+  const mesh = new THREE.Mesh(geometry, material);
+  return mesh;
+}
+
+function getPlane() {
+  const geometry = new THREE.PlaneGeometry(WORLD_HALF * 10, WORLD_HALF * 10, 100, 100);
+  const material = new THREE.MeshBasicMaterial({
+    wireframe: true,
+    side: THREE.DoubleSide
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+
+  //console.log(geometry.attributes.position.array);
+  let posArray = geometry.attributes.position.array;
+  for (let i = 0; i < posArray.length; i += 3) {
+    let x = posArray[i + 0];
+    let y = posArray[i + 1];
+    let z = posArray[i + 2];
+
+    let xOffset = (x + WORLD_HALF) * 0.02;
+    let yOffset = (y + WORLD_HALF) * 0.02;
+    let amp = 6;
+    let noiseValue = (noise(xOffset, yOffset) * amp) ** 3;
+
+    posArray[i + 2] = noiseValue; // update the z value.
+  }
+
+  return mesh;
+}
+
+///// p5.js /////
+
+function setup() {
+  let canvas = createCanvas(640, 480);
+  canvas.parent("container-p5");
+  canvas.hide();
+  background(50);
+
+  initTHREE();
+}
+
+function draw() {
+  noLoop();
+}
+
+///// three.js /////
+
+let container, stats, gui, params;
+let scene, camera, renderer, controls;
+let time = 0;
+let frame = 0;
+
+function initTHREE() {
+  // scene
+  scene = new THREE.Scene();
+
+  // camera (fov, ratio, near, far)
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.5,
+    5000
+  );
+  camera.position.z = 100;
+
+  // renderer
+  renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor("#333333");
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // container
+  container = document.getElementById("container-three");
+  container.appendChild(renderer.domElement);
+
+  // controls
+  controls = new THREE.PointerLockControls(camera, renderer.domElement);
+  //controls.lock();
+  scene.add(controls.getObject());
+
+  // gui
+  // https://davidwalsh.name/dat-gui
+  gui = new dat.gui.GUI();
+  params = {};
+
+  // stats
+  stats = new Stats();
+  stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+  container.appendChild(stats.dom);
+
+  setupTHREE();
+
+  // let's draw!
+  animate();
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  stats.update();
+  time = performance.now();
+  frame++;
+
+  updateTHREE();
+
+  render();
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
+
+// event listeners
+window.addEventListener("resize", onWindowResize);
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function onKeyDown(event) {
+  controls.lock(); // *** this should be triggered by user interaction
+  switch (event.code) {
+    case 'ArrowUp':
+    case 'KeyW':
+      moveForward = true;
+      break;
+    case 'ArrowLeft':
+    case 'KeyA':
+      moveLeft = true;
+      break;
+    case 'ArrowDown':
+    case 'KeyS':
+      moveBackward = true;
+      break;
+    case 'ArrowRight':
+    case 'KeyD':
+      moveRight = true;
+      break;
+    case 'Space':
+      if (canJump === true) jumpVelocity += jumpAccel;
+      canJump = false;
+      break;
+  }
+};
+
+function onKeyUp(event) {
+  switch (event.code) {
+    case 'ArrowUp':
+    case 'KeyW':
+      moveForward = false;
+      break;
+    case 'ArrowLeft':
+    case 'KeyA':
+      moveLeft = false;
+      break;
+    case 'ArrowDown':
+    case 'KeyS':
+      moveBackward = false;
+      break;
+    case 'ArrowRight':
+    case 'KeyD':
+      moveRight = false;
+      break;
+  }
+};
---
+++ w09_navigation/09_camera_lookAtVector/script.js
@@ -0,0 +1,180 @@
+const WORLD_HALF = 200;
+
+let plane;
+let cube;
+
+function setupTHREE() {
+  // plane
+  plane = getPlane();
+  plane.position.y = -WORLD_HALF / 2;
+  plane.rotation.x = -PI / 2;
+  scene.add(plane);
+
+  // boxes
+  for (let i = 0; i < 100; i++) {
+    let box = getBox();
+
+    box.position.x = random(-WORLD_HALF, WORLD_HALF);
+    box.position.y = random(-WORLD_HALF / 2, WORLD_HALF);
+    box.position.z = random(-WORLD_HALF, WORLD_HALF);
+
+    box.rotation.x = random(TWO_PI);
+    box.rotation.y = random(TWO_PI);
+    box.rotation.z = random(TWO_PI);
+
+    const size = random(1, 20);
+    box.scale.x = size;
+    box.scale.y = size;
+    box.scale.z = size;
+
+    box.material.transparent = true;
+    box.material.opacity = random(0.4, 0.7);
+
+    scene.add(box);
+  }
+
+  // cube
+  cube = getBox();
+  cube.scale.set(20, 20, 20);
+  cube.material.color.r = 1.0;
+  cube.material.color.g = 0.0;
+  cube.material.color.b = 1.0;
+  scene.add(cube);
+
+  // gui
+  gui.add(camera.position, "z", 10, 500).step(1);
+}
+
+function updateTHREE() {
+  camera.position.x = cos(frame * 0.010) * 100;
+  camera.lookAt(0, 0, 0);
+
+  let lookAtVector = new THREE.Vector3(0, 0, -1);
+  lookAtVector.applyQuaternion(camera.quaternion);
+  lookAtVector.multiplyScalar(300.0); // distance from the camera
+  let targetPos = new THREE.Vector3().addVectors(camera.position, lookAtVector);
+  cube.position.copy(targetPos);
+
+  cube.setRotationFromQuaternion(camera.quaternion);
+}
+
+function getBox() {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial();
+  const mesh = new THREE.Mesh(geometry, material);
+  return mesh;
+}
+
+function getPlane() {
+  const geometry = new THREE.PlaneGeometry(WORLD_HALF * 10, WORLD_HALF * 10, 100, 100);
+  const material = new THREE.MeshBasicMaterial({
+    wireframe: true,
+    side: THREE.DoubleSide
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+
+  //console.log(geometry.attributes.position.array);
+  let posArray = geometry.attributes.position.array;
+  for (let i = 0; i < posArray.length; i += 3) {
+    let x = posArray[i + 0];
+    let y = posArray[i + 1];
+    let z = posArray[i + 2];
+
+    let xOffset = (x + WORLD_HALF) * 0.02;
+    let yOffset = (y + WORLD_HALF) * 0.02;
+    let amp = 6;
+    let noiseValue = (noise(xOffset, yOffset) * amp) ** 3;
+
+    posArray[i + 2] = noiseValue; // update the z value.
+  }
+
+  return mesh;
+}
+
+///// p5.js /////
+
+function setup() {
+  let canvas = createCanvas(640, 480);
+  canvas.parent("container-p5");
+  canvas.hide();
+  background(50);
+
+  initTHREE();
+}
+
+function draw() {
+  noLoop();
+}
+
+///// three.js /////
+
+let container, stats, gui, params;
+let scene, camera, renderer;
+let time = 0;
+let frame = 0;
+
+function initTHREE() {
+  // scene
+  scene = new THREE.Scene();
+
+  // camera (fov, ratio, near, far)
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.5,
+    5000
+  );
+  camera.position.z = 100;
+
+  // renderer
+  renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor("#333333");
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // container
+  container = document.getElementById("container-three");
+  container.appendChild(renderer.domElement);
+
+  // controls
+  //let controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+  // gui
+  // https://davidwalsh.name/dat-gui
+  gui = new dat.gui.GUI();
+  params = {};
+
+  // stats
+  stats = new Stats();
+  stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+  container.appendChild(stats.dom);
+
+  setupTHREE();
+
+  // let's draw!
+  animate();
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  stats.update();
+  time = performance.now();
+  frame++;
+
+  updateTHREE();
+
+  render();
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
+
+// event listeners
+window.addEventListener("resize", onWindowResize);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
