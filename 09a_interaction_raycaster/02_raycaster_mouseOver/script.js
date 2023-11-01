41173851ac713024ec82cb731ee33b1a33754efc
MOQN <moonjunghyun@gmail.com>
2023-11-02 01:34:38 +0800

    update the folder structure

---
+++ 09a_interaction_raycaster/02_raycaster_mouseOver/script.js
@@ -0,0 +1,240 @@
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+let cubes = [];
+
+function setupTHREE() {
+  for (let i = 0; i < 100; i++) {
+    let tCube = new Cube()
+      .setPosition(random(-10, 10), random(-10, 10), random(-10, 10))
+      .setVelocity(random(-0.01, 0.01), random(-0.01, 0.01), random(-0.01, 0.01))
+      .setRotationVelocity(random(-0.01, 0.01), random(-0.01, 0.01), random(-0.01, 0.01))
+      .setScale(random(0.1, 1.5), random(0.1, 1.5), random(0.1, 1.5));
+    cubes.push(tCube);
+  }
+}
+
+function updateTHREE() {
+  // update the picking ray with the camera and mouse position
+  raycaster.setFromCamera(mouse, camera);
+  // calculate objects intersecting the picking ray
+  const intersections = raycaster.intersectObjects(scene.children);
+  /*
+  for (let i = 0; i < intersections.length; i++) {
+    let obj = intersections[i].object;
+    obj.material.color.set(0x00ff00);
+  }
+  */
+
+  for (let c of cubes) {
+    c.intersect(intersections);
+    c.move();
+    c.rotate();
+    c.update();
+  }
+}
+
+function getBox() {
+  let geometry = new THREE.BoxGeometry(1, 1, 1);
+  let material = new THREE.MeshBasicMaterial({
+    wireframe: true
+  });
+  let mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+  return mesh;
+}
+
+class Cube {
+  constructor() {
+    this.pos = createVector();
+    this.vel = createVector();
+    this.acc = createVector();
+    this.scl = createVector(1, 1, 1);
+    this.mass = this.scl.x * this.scl.y * this.scl.z;
+    this.rot = createVector();
+    this.rotVel = createVector();
+    this.rotAcc = createVector();
+    this.isSelected = false;
+    this.mesh = getBox();
+  }
+  setPosition(x, y, z) {
+    this.pos = createVector(x, y, z);
+    return this;
+  }
+  setVelocity(x, y, z) {
+    this.vel = createVector(x, y, z);
+    return this;
+  }
+  setRotationAngle(x, y, z) {
+    this.rot = createVector(x, y, z);
+    return this;
+  }
+  setRotationVelocity(x, y, z) {
+    this.rotVel = createVector(x, y, z);
+    return this;
+  }
+   setScale(w, h = w, d = w) {
+    const minScale = 0.01;
+    if (w < minScale) w = minScale;
+    if (h < minScale) h = minScale;
+    if (d < minScale) d = minScale;
+    this.scl = createVector(w, h, d);
+    this.mass = this.scl.x * this.scl.y * this.scl.z;
+    return this;
+  }
+  move() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+  rotate() {
+    this.rotVel.add(this.rotAcc);
+    this.rot.add(this.rotVel);
+    this.rotAcc.mult(0);
+  }
+  applyForce(f) {
+    let force = f.copy();
+    force.div(this.mass);
+    this.acc.add(force);
+  }
+  intersect(intersections) {
+    let isIntersected = false;
+    // if you only want to select the first (closest) one.
+    if (intersections.length > 0) {
+      if (this.mesh.uuid === intersections[0].object.uuid) {
+        isIntersected = true;
+      }
+    }
+    /*
+    // if you want to select the whole objects on the ray.
+    for (let i of intersections) {
+      if (this.mesh.uuid === i.object.uuid) {
+        isIntersected = true;
+      }
+    }
+    */
+    if (isIntersected) {
+      this.mesh.material.wireframe = false;
+      this.mesh.material.color.set(0x00ff00);
+    } else {
+      this.mesh.material.wireframe = true;
+      this.mesh.material.color.set(0xffffff);
+    }
+  }
+  update() {
+    this.mesh.position.set(this.pos.x, this.pos.y, this.pos.z);
+    this.mesh.rotation.set(this.rot.x, this.rot.y, this.rot.z);
+    this.mesh.scale.set(this.scl.x, this.scl.y, this.scl.z);
+  }
+}
+
+
+
+///// p5.js /////
+
+function setup() {
+  let canvas = createCanvas(640, 480);
+  canvas.parent("container-p5");
+  canvas.hide();
+  background(50);
+
+  initTHREE();
+}
+
+function draw() {
+  noLoop();
+}
+
+///// three.js /////
+
+let container, stats, gui, params;
+let scene, camera, renderer;
+let time = 0;
+let frame = 0;
+
+function initTHREE() {
+  console.log(THREE.REVISION);
+  
+  // scene
+  scene = new THREE.Scene();
+
+  // camera (fov, ratio, near, far)
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.5,
+    5000
+  );
+  camera.position.z = 100;
+
+  // renderer
+  renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor("#333333");
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // container
+  container = document.getElementById("container-three");
+  container.appendChild(renderer.domElement);
+
+  // controls
+  let controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+  // gui
+  // https://davidwalsh.name/dat-gui
+  gui = new dat.gui.GUI();
+  params = {
+    value1: 0,
+    value2: 0,
+    value3: 0
+  };
+  gui.add(params, "value1", -10, 10).step(1);
+  gui
+    .add(params, "value2")
+    .min(-10)
+    .max(10)
+    .step(10);
+  // .listen()
+  // .onChange(callback)
+  let folder = gui.addFolder("controls");
+  folder.add(params, "value3", -10, 10).step(1);
+
+  // stats
+  stats = new Stats();
+  stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+  container.appendChild(stats.dom);
+
+  setupTHREE();
+
+  // let's draw!
+  animate();
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  stats.update();
+  time = performance.now();
+  frame++;
+
+  updateTHREE();
+
+  renderer.render(scene, camera);
+}
+
+// event listeners
+
+window.addEventListener("resize", onWindowResize);
+window.addEventListener('mousemove', onMouseMove);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function onMouseMove(event) {
+  // calculate mouse position in normalized device coordinates
+  // (-1 to +1) for both components
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
