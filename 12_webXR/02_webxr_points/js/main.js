5f3f3c4ccd6ce42cc8c247871ec57b34a4c44dd5
MOQN <moonjunghyun@gmail.com>
2023-11-30 02:25:41 +0800

    update

--- 12_webXR/02_webxr_points/js/main.js
+++ 12_webXR/02_webxr_points/js/main.js
@@ -1,9 +1,12 @@
 let params = {
-  // (add)
+  drawCount: 0,
 };
 
+const MAX_PARTICLE_NUMBER = 5000;
+
 let room;
-let cube;
+let pointCloud;
+let particles = [];
 
 function setupThree() {
   // WebXR
@@ -13,26 +16,58 @@ function setupThree() {
   room = getRoom();
   scene.add(room);
 
-  // lights
-  const hemiLight = new THREE.HemisphereLight(0xa5a5a5, 0x898989, 3);
-  scene.add(hemiLight);
-
-  const direcLight = new THREE.DirectionalLight(0xffffff, 3);
-  direcLight.position.set(1, 1, 1).normalize();
-  scene.add(direcLight);
-
-  // cube
-  cube = getBox();
-  cube.position.set(0, 4, 0);
-  cube.scale.x = 1.5;
-  cube.scale.y = 1.5;
-  cube.scale.z = 1.5;
-  scene.add(cube);
+  // Points
+  pointCloud = getPoints(MAX_PARTICLE_NUMBER);
+  scene.add(pointCloud);
 }
 
 function updateThree() {
-  cube.rotation.x += 0.01;
-  cube.rotation.z += 0.005;
+  // generate more particles
+  if (particles.length < MAX_PARTICLE_NUMBER) {
+    let tParticle = new Particle()
+      .setPosition(random(-2, 2), 0, 0)
+      .setVelocity(random(-0.02, 0.02), random(-0.02, 0.02), random(-0.02, 0.02))
+    particles.push(tParticle);
+  }
+
+  // update the particles first
+  for (let i = 0; i < particles.length; i++) {
+    let p = particles[i];
+
+    //p.attractedTo(0, 0, 0);
+    p.flow();
+    p.move();
+    p.adjustVelocity(-0.005);
+    p.rotate();
+
+    p.age();
+    if (p.isDone) {
+      particles.splice(i, 1);
+      i--;
+    }
+  }
+
+  // then update the points
+  let positionArray = pointCloud.geometry.attributes.position.array;
+  let colorArray = pointCloud.geometry.attributes.color.array;
+  for (let i = 0; i < particles.length; i++) {
+    let p = particles[i];
+    let ptIndex = i * 3;
+    // position
+    positionArray[ptIndex + 0] = p.pos.x;
+    positionArray[ptIndex + 1] = p.pos.y;
+    positionArray[ptIndex + 2] = p.pos.z;
+    //color
+    colorArray[ptIndex + 0] = 1.0 * p.lifespan;
+    colorArray[ptIndex + 1] = 0.5 * p.lifespan;
+    colorArray[ptIndex + 2] = 0.1 * p.lifespan;
+  }
+  pointCloud.geometry.setDrawRange(0, particles.length); // ***
+  pointCloud.geometry.attributes.position.needsUpdate = true;
+  pointCloud.geometry.attributes.color.needsUpdate = true;
+
+  // update GUI
+  params.drawCount = particles.length;
 }
 
 
@@ -49,12 +84,131 @@ function getRoom() {
   return mesh;
 }
 
-function getBox() {
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshLambertMaterial({
-    color: Math.random() * 0xffffff
+function getPoints(number) {
+  const vertices = new Float32Array(number * 3);
+  const colors = new Float32Array(number * 3);
+
+  // geometry
+  const geometry = new THREE.BufferGeometry();
+  // attributes
+  geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+  geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+  // draw range
+  const drawCount = number; // draw the whole objects
+  geometry.setDrawRange(0, drawCount);
+  // geometry
+  const texture = new THREE.TextureLoader().load('assets/particle_texture.jpg');
+  const material = new THREE.PointsMaterial({
+    //color: 0xFF9911,
+    vertexColors: true,
+    size: 0.1,
+    sizeAttenuation: true, // default
+    opacity: 0.9,
+    transparent: true,
+    depthTest: false,
+    blending: THREE.AdditiveBlending,
+    map: texture
   });
-  const mesh = new THREE.Mesh(geometry, material);
-  return mesh;
+  // Points
+  const points = new THREE.Points(geometry, material);
+  return points;
 }
 
+class Particle {
+  constructor() {
+    this.pos = createVector();
+    this.vel = createVector();
+    this.acc = createVector();
+
+    this.scl = createVector(1, 1, 1);
+    this.mass = this.scl.x * this.scl.y * this.scl.z;
+
+    this.rot = createVector();
+    this.rotVel = createVector();
+    this.rotAcc = createVector();
+
+    this.lifespan = 1.0;
+    this.lifeReduction = random(0.001, 0.005);
+    this.isDone = false;
+  }
+  setPosition(x, y, z) {
+    this.pos = createVector(x, y, z);
+    return this;
+  }
+  setVelocity(x, y, z) {
+    this.vel = createVector(x, y, z);
+    return this;
+  }
+  setRotationAngle(x, y, z) {
+    this.rot = createVector(x, y, z);
+    return this;
+  }
+  setRotationVelocity(x, y, z) {
+    this.rotVel = createVector(x, y, z);
+    return this;
+  }
+  setScale(w, h = w, d = w) {
+    const minScale = 0.01;
+    if (w < minScale) w = minScale;
+    if (h < minScale) h = minScale;
+    if (d < minScale) d = minScale;
+    this.scl = createVector(w, h, d);
+    this.mass = this.scl.x * this.scl.y * this.scl.z;
+    return this;
+  }
+  move() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+  adjustVelocity(amount) {
+    this.vel.mult(1 + amount);
+  }
+  rotate() {
+    this.rotVel.add(this.rotAcc);
+    this.rot.add(this.rotVel);
+    this.rotAcc.mult(0);
+  }
+  applyForce(f) {
+    let force = f.copy();
+    force.div(this.mass);
+    this.acc.add(force);
+  }
+  reappear() {
+    if (this.pos.z > WORLD_SIZE / 2) {
+      this.pos.z = -WORLD_SIZE / 2;
+    }
+  }
+  disappear() {
+    if (this.pos.z > WORLD_SIZE / 2) {
+      this.isDone = true;
+    }
+  }
+  age() {
+    this.lifespan -= this.lifeReduction;
+    if (this.lifespan <= 0) {
+      this.lifespan = 0;
+      this.isDone = true;
+    }
+  }
+  attractedTo(x, y, z) {
+    let target = new p5.Vector(x, y, z);
+    let force = p5.Vector.sub(target, this.pos);
+    if (force.mag() < 100) {
+      force.mult(-0.005);
+    } else {
+      force.mult(0.0001);
+    }
+    this.applyForce(force);
+  }
+  flow() {
+    let xFreq = this.pos.x * 0.05 + frame * 0.005;
+    let yFreq = this.pos.y * 0.05 + frame * 0.005;
+    let zFreq = this.pos.z * 0.05 + frame * 0.005;
+    let noiseValue = map(noise(xFreq, yFreq, zFreq), 0.0, 1.0, -1.0, 1.0);
+    let force = new p5.Vector(cos(frame * 0.005), sin(frame * 0.005), sin(frame * 0.002));
+    force.normalize();
+    force.mult(noiseValue * 0.01);
+    this.applyForce(force);
+  }
+}
