c7841c5e3ed0bd24dab1c2af99b0470c1258e65e
MOQN <moonjunghyun@gmail.com>
2023-12-06 01:12:01 +0800

    update example code

--- 12_webXR/03_webxr_drag/js/script-webxr.js
+++ 12_webXR/03_webxr_drag/js/script-webxr.js
@@ -29,7 +29,10 @@ function setupWebXR() {
   // display the XR Button
   document.body.appendChild(XRButton.createButton(renderer));
 
-  // controllers and raycaster
+  // raycaster
+  raycaster = new THREE.Raycaster();
+
+  // visualize rays with lines
   const geometry = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, - 1)]);
 
   const line = new THREE.Line(geometry);
@@ -38,44 +41,69 @@ function setupWebXR() {
 
   controller1.add(line.clone());
   controller2.add(line.clone());
-
-  raycaster = new THREE.Raycaster();
 }
 
 function onSelectStart(event) {
-
   const controller = event.target;
-
   const intersections = getIntersections(controller);
 
   if (intersections.length > 0) {
-
     const intersection = intersections[0];
-
     const object = intersection.object;
     object.material.emissive.b = 1;
     controller.attach(object);
-
     controller.userData.selected = object;
-
   }
-
   controller.userData.targetRayMode = event.data.targetRayMode;
-
 }
 
 function onSelectEnd(event) {
-
   const controller = event.target;
-
   if (controller.userData.selected !== undefined) {
-
     const object = controller.userData.selected;
     object.material.emissive.b = 0;
     group.attach(object);
-
     controller.userData.selected = undefined;
-
   }
+}
 
+
+const intersected = [];
+const tempMatrix = new THREE.Matrix4();
+
+function getIntersections(controller) {
+  controller.updateMatrixWorld();
+  tempMatrix.identity().extractRotation(controller.matrixWorld);
+
+  raycaster.ray.origin.setFromMatrixPosition(controller.matrixWorld);
+  raycaster.ray.direction.set(0, 0, - 1).applyMatrix4(tempMatrix);
+
+  return raycaster.intersectObjects(group.children, false);
+}
+
+function intersectObjects(controller) {
+  // Do not highlight in mobile-ar
+  if (controller.userData.targetRayMode === 'screen') return;
+  // Do not highlight when already selected
+  if (controller.userData.selected !== undefined) return;
+
+  const line = controller.getObjectByName('line');
+  const intersections = getIntersections(controller);
+
+  if (intersections.length > 0) {
+    const intersection = intersections[0];
+    const object = intersection.object;
+    object.material.emissive.r = 1;
+    intersected.push(object);
+    line.scale.z = intersection.distance;
+  } else {
+    line.scale.z = 5;
+  }
+}
+
+function cleanIntersected() {
+  while (intersected.length) {
+    const object = intersected.pop();
+    object.material.emissive.r = 0;
+  }
 }
