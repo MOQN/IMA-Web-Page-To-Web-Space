1d2512747c036d81a54b1266f6e3c70ec5e82737
moqn <moonjunghyun@gmail.com>
2024-10-14 11:54:13 +0800

    add in-class code

---
+++ in-class/w04-demo/js/main.js
@@ -0,0 +1,147 @@
+let params = {
+  numberOfCubes: 0,
+  sceneChildren: 0
+};
+
+const WORLD_SIZE = 2000;
+let cubes = [];
+
+function setupThree() {
+  // GUI
+  gui.add(params, "numberOfCubes").listen();
+  gui.add(params, "sceneChildren").listen();
+
+  //
+  for (let i = 0; i < 3000; i++) {
+    let randomPos = p5.Vector.random3D();
+    randomPos.mult(WORLD_SIZE);
+
+    let tCube = new Cube()
+      .setPosition(randomPos.x, randomPos.y, randomPos.z)
+      .setVelocity(random(-1, 1), random(-1, 1), random(-1, 1))
+      .setScale(random(10, 50))
+    cubes.push(tCube);
+  }
+}
+
+function updateThree() {
+
+  for (let c of cubes) {
+    // let force = createVector(0, -0.5, 0);
+    // c.applyForce(force);
+
+    c.move();
+    c.age();
+    c.update();
+  }
+
+  for (let i = 0; i < cubes.length; i++) {
+    let c = cubes[i];
+    if (c.isDone) {
+      // remove!
+      scene.remove(c.mesh); // remove the mesh from the scene first
+      cubes.splice(i, 1); // then remove the object
+    }
+  }
+
+  params.numberOfCubes = cubes.length;
+  params.sceneChildren = scene.children.length;
+}
+
+function getBox() {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshNormalMaterial({
+    // color: 0xffffff,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  return mesh;
+}
+
+class Cube {
+  constructor() {
+    this.pos = createVector();
+    this.vel = createVector();
+    this.acc = createVector();
+
+    this.scl = createVector(1, 1, 1);
+    this.mass = 1; //this.scl.x * this.scl.y * this.scl.z;
+
+    this.rot = createVector();
+    this.rotVel = createVector();
+    this.rotAcc = createVector();
+
+    this.lifespan = 1.0;
+    this.lifeReduction = random(0.005, 0.010);
+    this.isDone = false;
+
+    this.mesh = getBox();
+    scene.add(this.mesh); // don't forget to add to scene
+  }
+  setPosition(x, y, z) {
+    this.pos = createVector(x, y, z);
+    return this;
+  }
+  setVelocity(x, y, z) {
+    this.vel = createVector(x, y, z);
+    return this;
+  }
+  setRotationAngle(x, y, z) {
+    this.rot = createVector(x, y, z);
+    return this;
+  }
+  setRotationVelocity(x, y, z) {
+    this.rotVel = createVector(x, y, z);
+    return this;
+  }
+  setScale(w, h = w, d = w) {
+    const minScale = 0.01;
+    if (w < minScale) w = minScale;
+    if (h < minScale) h = minScale;
+    if (d < minScale) d = minScale;
+    this.scl = createVector(w, h, d);
+    this.mass = this.scl.x * this.scl.y * this.scl.z;
+    this.mass = 1;
+    return this;
+  }
+  move() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+  rotate() {
+    this.rotVel.add(this.rotAcc);
+    this.rot.add(this.rotVel);
+    this.rotAcc.mult(0);
+  }
+  applyForce(f) {
+    let force = f.copy();
+    force.div(this.mass);
+    this.acc.add(force);
+  }
+  reappear() {
+    if (this.pos.z > WORLD_SIZE / 2) {
+      this.pos.z = -WORLD_SIZE / 2;
+    }
+  }
+  disappear() {
+    if (this.pos.z > WORLD_SIZE / 2) {
+      this.isDone = true;
+    }
+  }
+  age() {
+    this.lifespan -= this.lifeReduction;
+    if (this.lifespan <= 0) {
+      this.lifespan = 0;
+      this.isDone = true;
+    }
+  }
+  update() {
+    this.mesh.position.set(this.pos.x, this.pos.y, this.pos.z);
+    this.mesh.rotation.set(this.rot.x, this.rot.y, this.rot.z);
+
+    let newScale = p5.Vector.mult(this.scl, this.lifespan);
+    this.mesh.scale.set(newScale.x, newScale.y, newScale.z);
+  }
+}
+
+
